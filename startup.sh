46938b8f8e14702379366a7eb077af15f08558d8
Talhaisteaqq <85591575+AnossaTG@users.noreply.github.com>
2021-11-18 12:22:01 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
-# Copyright (C) 2020-2021 by AGithub, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by AnossaTG@Github, < https://github.com/AnossaTG >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/AnossaTG/ElectronUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < hhttps://github.com/AnossaTG/ElectronUserBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
