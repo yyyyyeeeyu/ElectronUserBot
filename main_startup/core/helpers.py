5d99924d5b155ac9b7039743fcc22ce49bda36eb
Talhaisteaqq <85591575+AnossaTG@users.noreply.github.com>
2021-11-18 23:57:08 +0200

    Electron

---
+++ main_startup/core/helpers.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2020-2021 by AnossaTG@Github, < https://github.com/AnossaTG >.
+#
+# This file is part of < https://github.com/AnossaTG/ElectronUserBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/AnossaTG/ElectronUserBot/blob/master/LICENSE >
+#
+# All rights reserved.
+from main_startup.config_var import Config
+from main_startup.helper_func.basic_helpers import edit_or_reply, is_admin_or_owner
